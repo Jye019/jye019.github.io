17e7b011eb4bf7d286072b4dab5b33cdce7fa460
jye019 <jihyekim019@gmail.com>
2021-01-31 11:51:40 +0900

    config  설정

--- Blog/gatsby-config.js
+++ Blog/gatsby-config.js
@@ -1,15 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `imzeze Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Kim Jihye`,
+      summary: `tech blog`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
-    social: {
-      twitter: `kylemathews`,
-    },
   },
   plugins: [
     {
